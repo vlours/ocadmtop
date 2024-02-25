490db49d5f93f05fa479e919f4daafb89cc92c33
Vincent Lours <42592162+vlours@users.noreply.github.com>
2024-02-26 09:04:54 +1000

    Version 0.4 - Implement timeout option as requested in issue #1 - Reorder 'getopts' command to match the help order - README: fix all markdown issues

--- ocadmtop_node.sh
+++ ocadmtop_node.sh
@@ -3,11 +3,12 @@
 # Owner       # Red Hat - CEE
 # Name        # ocadmtop_node.sh
 # Description # Script to display detailed POD CPU/MEM usage on node
+# @VERSION    # 0.4
 ########################################################################
 
 #### Functions
 fct_usage() {
-echo "$(basename $0) [-c|-m|-p] [-A|-L <label1>,<label2>,...|-H <host1>,<host2>,...] [-d {0-10}] [-v|-h]
+echo "$(basename $0) [-c|-m|-p] [-A|-L <label1>,<label2>,...|-H <host1>,<host2>,...] [-d {0-10}] [-t <TIMEOUT>][-v|-h]
   -c: sort by CPU (default)
   -m: sort by Memory
   -p: sort by namespace/pod
@@ -15,14 +16,19 @@ echo "$(basename $0) [-c|-m|-p] [-A|-L <label1>,<label2>,...|-H <host1>,<host2>,
   -H: retrieve node(s) by hostname
   -A: retrieve All nodes (default)
   -d: debug/loglevel mode. Provide additional 'oc --loglevel' ouput. (Recommended value: 6)
+  -t: The length of time to wait before giving up on a single server request. Non-zero values should contain a
+      corresponding time unit (e.g. 1s, 2m, 3h). A value of zero means don't timeout requests.
   -v: Display the version
   -h: Display this help"
 fct_version
 }
 
 fct_version() {
-echo "$(basename $0) - Version: ${VERSION}"
-exit 1
+  Script=$(which $0 2>${STD_ERR})
+  VERSION=$(grep "@VERSION" ${Script} 2>${STD_ERR} | grep -Ev "VERSION=" | cut -d'#' -f3)
+  VERSION=${VERSION:-" N/A"}
+  echo "$(basename $0) - Version: ${VERSION}"
+  exit 1
 }
 
 fct_cpu() {
@@ -41,33 +47,36 @@ fct_pod() {
 }
 
 #### MAIN
-VERSION=0.3
+VERSION=0.4
 {
-while getopts "cd:mpL:H:Avh" option;do
+while getopts "cmpL:H:Ad:t:vh" option;do
   case ${option} in
-    d) if [[ ${OPTARG} =~ ^[1-9]$ ]] || [[ ${OPTARG} == 10 ]]; then LOGLEVEL="--loglevel ${OPTARG:-6}"; else fct_usage; fi ;;
     c|m|p) SORT=${option} ;;
-    H) NODES=$(echo ${OPTARG} |sed -e "s!,! !") ;;
     L) NODE_OPT=L && NODE_ARG=${OPTARG} ;;
+    H) NODES=$(echo ${OPTARG} |sed -e "s!,! !") ;;
     A) NODE_OPT=A ;;
+    d) if [[ ${OPTARG} =~ ^[1-9]$ ]] || [[ ${OPTARG} == 10 ]]; then LOGLEVEL="--loglevel ${OPTARG:-6}"; else fct_usage; fi ;;
+    t) TIMEOUT=${OPTARG:-"1m"} ;;
     v) fct_version ;;
     h|*) fct_usage ;;
   esac
 done
+TIMEOUT=${TIMEOUT:-"1m"}
+OC="oc --request-timeout=${TIMEOUT}"
 
 if [[ -z $NODES ]]
 then
   case ${NODE_OPT} in
-    L)  NODES=$(oc get nodes -l ${NODE_ARG} -o name ${LOGLEVEL} | cut -d'/' -f2) ;;
-    *)  NODES=$(oc get nodes -o name ${LOGLEVEL} | cut -d'/' -f2) ;;
+    L)  NODES=$(${OC} get nodes -l ${NODE_ARG} -o name ${LOGLEVEL} | cut -d'/' -f2) ;;
+    *)  NODES=$(${OC} get nodes -o name ${LOGLEVEL} | cut -d'/' -f2) ;;
   esac
 fi
 
-RESOURCES=$(oc adm top pod -A ${LOGLEVEL})
+RESOURCES=$(${OC} adm top pod -A ${LOGLEVEL})
 for NODE in ${NODES}
 do
   echo -e "\n===== ${NODE} ====="
-  PODS=$(oc describe node ${NODE} ${LOGLEVEL} | awk '/Non-terminated Pods:/,/Allocated resources:/{if(($2 != "Pods:") && ($2 != "resources:") && ($2 != "----") && ($2 != "Name")){print $2}}')
+  PODS=$(${OC} describe node ${NODE} ${LOGLEVEL} | awk '/Non-terminated Pods:/,/Allocated resources:/{if(($2 != "Pods:") && ($2 != "resources:") && ($2 != "----") && ($2 != "Name")){print $2}}')
   case ${SORT} in
     m) fct_mem | column -s'|' -t ;;
     p) fct_pod | column -s'|' -t ;;
